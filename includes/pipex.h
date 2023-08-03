dcc26f66c600d8b9e639fdc633a896c983d8fcea
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-08-03 18:49:24 +0200

    geting the binaries

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: vzhadan <vzhadan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 18:06:43 by vzhadan           #+#    #+#             */
-/*   Updated: 2023/08/02 19:16:08 by vzhadan          ###   ########.fr       */
+/*   Updated: 2023/08/03 18:56:37 by vzhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,11 @@
 
 # define PIPE_IN 0
 # define PIPE_OUT 1
+
+/*
+ * returns an array of possible paths to the comands 
+ * from the PATH variable in the environment
+*/
+char **get_binaries(char **env);
+
 #endif
--- src/utils.c
+++ src/utils.c
@@ -6,9 +6,19 @@
 /*   By: vzhadan <vzhadan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 20:42:00 by vzhadan           #+#    #+#             */
-/*   Updated: 2023/08/02 20:44:15 by vzhadan          ###   ########.fr       */
+/*   Updated: 2023/08/03 18:48:42 by vzhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+char **get_binaries(char **env){
+	char **paths;
+	int i;
+	
+	i = 0;
+	while (env[++i])
+		if (ft_strnstr(env[i], "PATH=", 5))
+			return ft_split(env[i] + 5, ':');
+	return (paths);
+}
