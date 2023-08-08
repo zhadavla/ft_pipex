a634cfe8eddf600fb02c9ad289f8581b5e5dedc4
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-08-08 18:09:24 +0200

    freesplit

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -27,25 +27,33 @@ void heredoc(t_pipex *pipex) {
     }
 }
 
-void do_fork_names(t_pipex *pipex, int mode, char *cmd, char **env) {
-    if (fork() == 0) {
+int do_fork_names(t_pipex *pipex, int mode, char *cmd, char **env) {
+    int x = fork();
+    int dup_check = 1;
+    if (x == 0) {
         if (mode == FIRST_FORK) {
             if (pipex->is_heredoc)
-                ft_dup2(pipex->fd_pipes[0], pipex->fd_pipes[3]);
+                dup_check = ft_dup2(pipex->fd_pipes[0], pipex->fd_pipes[3]);
             else
-                ft_dup2(pipex->infile_fd, pipex->fd_pipes[1]);
+                dup_check = ft_dup2(pipex->infile_fd, pipex->fd_pipes[1]);
         }
         if (mode == MID_FORK)
-            ft_dup2(pipex->fd_pipes[pipex->fd_ind], pipex->fd_pipes[pipex->fd_ind + 3]);
+            dup_check = ft_dup2(pipex->fd_pipes[pipex->fd_ind], pipex->fd_pipes[pipex->fd_ind + 3]);
         if (mode == LAST_FORK) {
             if (pipex->is_heredoc)
-                ft_dup2(pipex->fd_pipes[2], pipex->outfile_fd);
+                dup_check = ft_dup2(pipex->fd_pipes[2], pipex->outfile_fd);
             else
-                ft_dup2(pipex->fd_pipes[pipex->fd_ind], pipex->outfile_fd);
+                dup_check = ft_dup2(pipex->fd_pipes[pipex->fd_ind], pipex->outfile_fd);
         }
         close_fd(pipex);
+        if (dup_check == -1)
+            return dup_check;
         ft_execute(cmd, env);
+    } else if (x == -1) {
+        perror("");
+        return -1;
     }
+    return 1;
 }
 
 int open_file(char *filename, bool is_heredoc, bool is_input) {
@@ -57,33 +65,36 @@ int open_file(char *filename, bool is_heredoc, bool is_input) {
         return open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
 }
 
-void execute_command(t_pipex *pipex, int num_of_commands, char **list_of_commands, char **env) {
+int execute_command(t_pipex *pipex, int num_of_commands, char **list_of_commands, char **env) {
     int j;
 
     if (pipex->infile_name)
         pipex->infile_fd = open_file(pipex->infile_name, pipex->is_heredoc, 1);
     pipex->outfile_fd = open_file(pipex->outfile_name, pipex->is_heredoc, 0);
 
-    if ((pipex->infile_name && pipex->infile_fd == -1) || pipex->outfile_fd == -1)
-    {
+    if ((pipex->infile_name && pipex->infile_fd == -1) || pipex->outfile_fd == -1) {
         perror("Error when opening file");
-        return;
+        return -1;
     }
     if (pipex->is_heredoc)
         heredoc(pipex);
     j = 0;
+    int is_error;
     pipex->fd_ind = 0;
     while (j < num_of_commands) {
         if (j == 0)
-            do_fork_names(pipex, FIRST_FORK, list_of_commands[j], env);
+            is_error = do_fork_names(pipex, FIRST_FORK, list_of_commands[j], env);
         else if (j == num_of_commands - 1)
-            do_fork_names(pipex, LAST_FORK, list_of_commands[j], env);
+            is_error = do_fork_names(pipex, LAST_FORK, list_of_commands[j], env);
         else {
-            do_fork_names(pipex, MID_FORK, list_of_commands[j], env);
+            is_error = do_fork_names(pipex, MID_FORK, list_of_commands[j], env);
             pipex->fd_ind += 2;
         }
         j++;
+        if (is_error == -1)
+            return -1;
     }
+    return 1;
 }
 
 char **create_list_of_commands(char **argv, int argc, t_pipex *pipex) {
@@ -99,11 +110,39 @@ char **create_list_of_commands(char **argv, int argc, t_pipex *pipex) {
     return commands;
 }
 
-bool argv_check(char *str, char **env)
+
+
+void free_split(char **strs)
 {
+    int i = 0;
+    while (strs[i]){
+        free(strs[i]);
+        i++;
+    }
+    free(strs);
+}
+
+char	*ft_join(char const *s1, char const *s2)
+{
+    size_t	len1;
+    size_t	len2;
+    char	*str;
+
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    str = (char *)malloc(sizeof(char) * (len1 + len2) + 1);
+    if (!s1 || !s2)
+        return (NULL);
+    if (!str)
+        return (NULL);
+    ft_strlcpy(str, s1, len1 + 1);
+    ft_strlcat(str, s2, len2 + len1 + 1);
+    return (str);
+}
+
+bool command_check(char *str, char **env) {
     char **cmd;
     char **path;
-
     char *pathname;
     int i;
 
@@ -111,56 +150,75 @@ bool argv_check(char *str, char **env)
     path = get_binaries(env);
     i = 0;
     while (path[i] != NULL) {
-        pathname = ft_strjoin(path[i], "/");
+        pathname = ft_join(path[i], "/");
         pathname = ft_strjoin(pathname, cmd[0]);
-        if (access(pathname, X_OK) == 0)
-        {
-            i = 0;
-            while (cmd[i++])
-                free(cmd[i]);
+        if (access(pathname, X_OK) == 0) {
             free(pathname);
+            free_split(cmd);
+            free_split(path);
             return true;
         }
-        i++;
+        free(pathname);
+
+        i++;        
     }
-    i = 0;
-    while (cmd[i++])
-        free(cmd[i]);
+//    free(pathname);
+    free_split(cmd);
+    free_split(path);
     return false;
 }
 
+int check_argv(char **argv, int argc, t_pipex *pipex, char **env) {
+    (void )pipex;
+    int i = 2;
+    if (pipex->is_heredoc)
+        i = 3;
+    while (i < argc - 1) {
+        printf("%s\n", argv[i]);
+        if (!command_check(argv[i], env))
+            return -1;
+        i++;
+    }
+    return 1;
+}
+
 int main(int argc, char **argv, char **env) {
-    int status;
-    char **list_of_commands;
+//    int status;
+//    char **list_of_commands;
     t_pipex pipex;
 
     if (argc < 5) {
         return (1);
     }
 
-//    if (!argv_check(argv, env)) {
-//        perror("");
-//        exit(EXIT_FAILURE);
+    if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+        pipex.is_heredoc = true;
+    if (check_argv(argv, argc, &pipex, env) == -1) {
+        perror("");
+        return (EXIT_FAILURE);
+    }
+//
+//    init_pipex(&pipex, argc, argv);
+//    int i = 0;
+//    while (i < pipex.fd_pipes_count) {
+//        if (pipe(pipex.fd_pipes + i) == -1)
+//            return (EXIT_FAILURE);
+//        i += 2;
 //    }
-
-    init_pipex(&pipex, argc, argv);
-
-    int i = 0;
-    while (i < pipex.fd_pipes_count) {
-        pipe(pipex.fd_pipes + i);
-        i += 2;
-    }
-    list_of_commands = create_list_of_commands(argv, argc, &pipex);
-    execute_command(&pipex, pipex.cmd_count, list_of_commands, env);
-    close_fd(&pipex);
-    if (pipex.infile_name)
-        close(pipex.infile_fd);
-    close(pipex.outfile_fd);
-    i = 0;
-    while (i < pipex.cmd_count) {
-        wait(&status);
-        i++;
-    }
-    free(pipex.fd_pipes);
+//    list_of_commands = create_list_of_commands(argv, argc, &pipex);
+//    if (execute_command(&pipex, pipex.cmd_count, list_of_commands, env) == -1) {
+//        perror("");
+//        return (EXIT_FAILURE);
+//    }
+//    close_fd(&pipex);
+//    if (pipex.infile_name)
+//        close(pipex.infile_fd);
+//    close(pipex.outfile_fd);
+//    i = 0;
+//    while (i < pipex.cmd_count) {
+//        wait(&status);
+//        i++;
+//    }
+//    free(pipex.fd_pipes);
     return (0);
 }
--- bonus/utils_bonus.c
+++ bonus/utils_bonus.c
@@ -12,26 +12,21 @@
 
 #include "../includes/pipex.h"
 
-void init_pipex(t_pipex *pipex, int argc, char **argv)
-{
-    if (ft_strncmp(argv[1], "here_doc", 8) == 0)
-    {
+void init_pipex(t_pipex *pipex, int argc, char **argv) {
+    if (ft_strncmp(argv[1], "here_doc", 8) == 0) {
         pipex->infile_name = NULL;
         pipex->limiter = argv[2];
         pipex->fd_pipes_count = 4;
         pipex->is_heredoc = true;
         pipex->cmd_count = 2;
-
-    }
-    else
-    {
+    } else {
         pipex->cmd_count = argc - 3;
         pipex->infile_name = argv[1];
         pipex->fd_pipes_count = (pipex->cmd_count - 1) * 2;
         pipex->is_heredoc = false;
     }
     pipex->outfile_name = argv[argc - 1];
-    pipex->fd_pipes = (int*)malloc(sizeof(int) * pipex->fd_pipes_count);
+    pipex->fd_pipes = (int *) malloc(sizeof(int) * pipex->fd_pipes_count);
 }
 
 void close_fd(t_pipex *pipex) {
@@ -44,9 +39,12 @@ void close_fd(t_pipex *pipex) {
     }
 }
 
-void ft_dup2(int fd1, int fd2) {
-    dup2(fd1, 0);
-    dup2(fd2, 1);
+int ft_dup2(int fd1, int fd2) {
+    if (dup2(fd1, 0) == -1)
+        return -1;
+    if (dup2(fd2, 1) == -1)
+        return -1;
+    return 1;
 }
 
 char **get_binaries(char **env) {
@@ -59,22 +57,27 @@ char **get_binaries(char **env) {
     return (NULL);
 }
 
-void ft_execute(char *argv, char **env) {
+bool ft_execute(char *str, char **env) {
     char **cmd;
     char **path;
-
     char *pathname;
     int i;
 
-    cmd = ft_split(argv, ' ');
+    cmd = ft_split(str, ' ');
     path = get_binaries(env);
     i = 0;
     while (path[i] != NULL) {
-        pathname = ft_strjoin(path[i], "/");
+        pathname = ft_join(path[i], "/");
         pathname = ft_strjoin(pathname, cmd[0]);
-        if (access(pathname, X_OK) == 0)
+        if (access(pathname, X_OK) == 0) {
+            free_split(path);
             execve(pathname, cmd, env);
+        }
         i++;
     }
+    free(pathname);
+    free_split(cmd);
+    free_split(path);
+    return false;
 }
 
