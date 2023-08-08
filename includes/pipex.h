05e4e5fc0cc4d9dcfe8a1f712ec956ac737d3fb7
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-08-08 19:40:54 +0200

    header norm

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,20 +6,20 @@
 /*   By: vzhadan <vzhadan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 18:06:43 by vzhadan           #+#    #+#             */
-/*   Updated: 2023/08/08 15:30:44 by vzhadan          ###   ########.fr       */
+/*   Updated: 2023/08/08 19:40:39 by vzhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "libft.h"
 # include "ft_printf.h"
-# include <sys/types.h>
-# include <sys/wait.h>
+# include "libft.h"
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 # include <unistd.h>
 
 # define PIPE_IN 0
@@ -29,35 +29,34 @@
 # define MID_FORK 2
 # define LAST_FORK 3
 
-# define true 1 
-# define false 0
-# define bool int
+# define TRUE 1
+# define FALSE 0
+# define BOOL int
 
 typedef struct s_pipex
 {
-    int fd_pipes_count;
-    int cmd_count;
-    int infile_fd;
-    int outfile_fd;
-    int *fd_pipes;
-    int fd_ind;
-    bool is_heredoc; 
-    char *infile_name;
-    char *outfile_name;
-    char *limiter;
-}   t_pipex;
+	int		fd_pipes_count;
+	int		cmd_count;
+	int		infile_fd;
+	int		outfile_fd;
+	int		*fd_pipes;
+	int		fd_ind;
+	BOOL	is_heredoc;
+	char	*infile_name;
+	char	*outfile_name;
+	char	*limiter;
+}			t_pipex;
 
 /*
- * returns an array of possible paths to the comands 
+ * returns an array of possible paths to the comands
  * from the PATH variable in the environment
-*/
-char **get_binaries(char **env);
-bool ft_execute(char *argv, char **env);
-char	*ft_join(char const *s1, char const *s2);
-int    ft_dup2(int fd1, int fd2);
-void    close_fd(t_pipex *pipex);
-void free_split(char **strs);
-void init_pipex(t_pipex *pipex, int argc, char **argv);
-
+ */
+char		**get_binaries(char **env);
+BOOL		ft_execute(char *argv, char **env);
+char		*ft_join(char const *s1, char const *s2);
+int			ft_dup2(int fd1, int fd2);
+void		close_fd(t_pipex *pipex);
+void		free_split(char **strs);
+void		init_pipex(t_pipex *pipex, int argc, char **argv);
 
 #endif
